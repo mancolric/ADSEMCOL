46ce005db0066cee08f75393b6df3da60eaabeca
mancolric <mancolric@gmail.com>
2025-12-27 17:51:13 +0100

    update

--- test/test_CompressibleFlow/Launch.jl
+++ test/test_CompressibleFlow/Launch.jl
@@ -22,7 +22,7 @@ try
         beta            = parse(Float64, ARGS[5])
         ML              = parse(Float64, ARGS[6])
         epsilon         = parse(Float64, ARGS[7])
-        Lx              = parse(Float64, ARGS[7])
+        Lx              = parse(Float64, ARGS[8])
         SC              = parse(Int, ARGS[9])
         ShockFlame_FXP(FesOrder=FesOrder, tf=tf, q=q, 
             RThatI=beta, RThatB=beta, ML=ML, epsilon=epsilon, 
