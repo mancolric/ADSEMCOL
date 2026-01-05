d550d94ec50615611730a3f868219230fef148bd
mancolric <mancolric@gmail.com>
2026-01-05 13:17:11 +0100

    fix gc

--- test/test_CompressibleFlow/LaunchParallel.sh
+++ test/test_CompressibleFlow/LaunchParallel.sh
@@ -5,11 +5,11 @@
 # NOTE: SBATCH parameters must be defined first. Do not define any variable before them.
 
 # Editable options:
-#SBATCH --job-name=LP2
+#SBATCH --job-name=LP3
 #SBATCH --partition=cn1
-#SBATCH --array=1-11%11
+#SBATCH --array=1-2%2
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=20G
+#SBATCH --mem=50G
 
 # Do not modify:
 #SBATCH --nodes=1
